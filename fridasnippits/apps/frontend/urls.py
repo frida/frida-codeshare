2cde987f4c7db7cbd47053b8a52160b1404aebb9
fitblip <fitblip@gmail.com>
2017-07-14 11:31:12 -0700

    Allow hyphens in all urls

--- fridasnippits/apps/frontend/urls.py
+++ fridasnippits/apps/frontend/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^new/?', views.new),
     url(r'^browse/?', views.browse),
-    url(r'^(?P<nickname>\@\w[\w-]*)/(?P<project_slug>[a-zA-Z0-9\-]+)/edit/?', views.project_edit, name='project_edit'),
-    url(r'^(?P<nickname>\@\w[\w-]*)/(?P<project_slug>[a-zA-Z0-9\-]+)/', views.project_view, name='project_view'),
-    url(r'^(?P<nickname>\@\w[\w-]*)/?$', views.user_info_view, name='user_info_view'),
+    url(r'^(?P<nickname>\@[\w\-]+)/(?P<project_slug>[a-zA-Z0-9\-]+)/edit/?', views.project_edit, name='project_edit'),
+    url(r'^(?P<nickname>\@[\w\-]+)/(?P<project_slug>[a-zA-Z0-9\-]+)/', views.project_view, name='project_view'),
+    url(r'^(?P<nickname>\@[\w\-]+)/?$', views.user_info_view, name='user_info_view'),
 ]
